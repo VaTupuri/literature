d08e4877abeeda6805ae3de5a9568cf2373303d7
Varun Tupuri <tupurigithub@gmail.com>
2024-09-28 23:06:59 -0500

    update api

--- literature-client/src/app/utils/api.ts
+++ literature-client/src/app/utils/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { Player } from '@/app/types';
 
 
-export const API_BASE_URL = 'http://10.183.76.168:5000'; // Replace with your actual API base URL
+export const API_BASE_URL = 'https://lit-game-api-79df797270b6.herokuapp.com'; // Replace with your actual API base URL
 
 export const createRoom = async (playerName: string) => {
   const response = await fetch(`${API_BASE_URL}/create_room`, {
